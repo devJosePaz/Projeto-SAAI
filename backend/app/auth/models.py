5388080a4595a304ef2511e01d8d54977130d690
José Paz <lusjos40@gmail.com>
2025-07-03 23:17:48 -0300

    criado a classe User no models do auth

---
+++ backend/app/auth/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False, unique=True)
+    hash_password = Column(String, nullable=False)
+    create_at = Column(DateTime(timezone=True), server_default=func.now())
